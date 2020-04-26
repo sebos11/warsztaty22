e8749a1748abe39000818bbbd0c4229854212691
sebos11 <sszast@gmail.com>
2020-04-26 21:45:34 +0200

    zarządzanie użytkownikami dodano

--- src/pl/coderslab/app/manageUsers.java
+++ src/pl/coderslab/app/manageUsers.java
@@ -20,18 +20,62 @@ public class manageUsers {
             switch (userAnswer) {
                 case 1: {
                     System.out.println("DODAWANIE UŻYTKOWNIKA");
-
+                    String text;
+                    User user = new User();
+                    UserDao userDao = new UserDao();
+                    Scanner scan = new Scanner(System.in);
+                    System.out.print("podaj nazwę użytkownika: ");
+                    text = scan.next();
+                    user.setUserName(text);
+                    System.out.print("podaj email użytkownika: ");
+                    text = scan.next();
+                    user.setEmail(text);
+                    System.out.print("podaj hasło użytkownika: ");
+                    text = scan.next();
+                    user.setPassword(text);
+                    System.out.print("podaj ID grupy użytkownika: ");
+                    int id = wczytajLiczbe();
+                    user.setUserGroupId(id);
+                    userDao.create(user);
+                    listUsers();
                     break;
                 }
                  case 2: {
+                     Scanner scan = new Scanner(System.in);
+                     String tekst;
                      System.out.println("EDYCJA UŻYTKOWNIKA");
+                     listUsers();
+                     System.out.print("Podaj id użytkownika którego chcesz zmienić >>>> ");
+                     int id = wczytajLiczbe();
+                     UserDao userDao = new UserDao();
+                     User user = userDao.read(id);
+                     User user1 = user;
+                     System.out.print("Zmień nazwę użytkownika z " +user.getUserName() + "  na  " );
+                     tekst = scan.next();
+                     user.setUserName(tekst);
+                     System.out.print("Zmień email użytkownika z " +user.getEmail() + "  na  " );
+                     tekst = scan.next();
+                     user.setEmail(tekst);
+                     System.out.print("Zmień hasło użytkownika z " +user.getPassword() + "  na  " );
+                     tekst = scan.next();
+                     user.setPassword(tekst);
+                     System.out.print("Zmień ID grupy użytkownika z " +user.getUserGroupId() + "  na  " );
+                     int idGroup = wczytajLiczbe();
+                     user.setUserGroupId(idGroup);
+                     userDao.update(user);
+                     System.out.print("Zmieniono dane użytkownika  " + user1.toString() + " na " );
+                     System.out.println(user.toString());
+
+
+
+
 
                    break;
                  }
                 case 3:
                     System.out.println("USUWANIE UŻYTKOWNIKA");
                     listUsers();
-                    System.out.println("Podaj id użytkownika którego chcesz usunąć >>>> ");
+                    System.out.print("Podaj id użytkownika którego chcesz usunąć >>>> ");
                     int id = wczytajLiczbe();
                     UserDao userDao = new UserDao();
                     userDao.delete(id);
